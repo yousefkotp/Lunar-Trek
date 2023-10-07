2c7bc6d74f433ccfe81814d37bb6be5172c3c4f2
MohammadHelaly <mohammad.helaly@gmail.com>
2023-10-07 15:33:51 +0300

    Add contact section

--- lunar-trek/src/App.js
+++ lunar-trek/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Background from "./components/Background/Background";
+import ContactSection from "./components/ContactSection/ContactSection";
 import ExploreSection from "./components/ExploreSection/ExploreSection";
 import Footer from "./components/Footer/Footer";
 import HomeSection from "./components/HomeSection/HomeSection";
@@ -10,6 +11,7 @@ const App = () => {
 			<Background />
 			<HomeSection />
 			<ExploreSection />
+			<ContactSection />
 			<Footer />
 		</>
 	);
