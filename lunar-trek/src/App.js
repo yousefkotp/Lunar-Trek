a9735c6360674f77c8a5a06250f4ef52ccc29eec
MohammadHelaly <mohammad.helaly@gmail.com>
2023-10-07 14:29:45 +0300

    Add background image

--- lunar-trek/src/App.js
+++ lunar-trek/src/App.js
@@ -1,22 +1,8 @@
 import "./App.css";
+import Background from "./components/Background/Background";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+	return <Background />;
 }
 
 export default App;
---
+++ lunar-trek/src/components/Background/Background.js
@@ -0,0 +1,7 @@
+import styles from "./Background.module.css";
+
+const Background = () => {
+	return <div className={styles["background"]} />;
+};
+
+export default Background;
