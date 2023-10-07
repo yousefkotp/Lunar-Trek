9be495882afa6a148b5ec14a66e6748cf0a17505
MohammadHelaly <mohammad.helaly@gmail.com>
2023-10-07 16:14:42 +0300

    Add initial navbar

--- lunar-trek/src/App.js
+++ lunar-trek/src/App.js
@@ -5,11 +5,13 @@ import ContactSection from "./components/ContactSection/ContactSection";
 import ExploreSection from "./components/ExploreSection/ExploreSection";
 import Footer from "./components/Footer/Footer";
 import HomeSection from "./components/HomeSection/HomeSection";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
 	return (
 		<>
 			<Background />
+			<NavBar />
 			<HomeSection />
 			<ExploreSection />
 			<AboutSection />
---
+++ lunar-trek/src/components/NavBar/NavBar.js
@@ -0,0 +1,76 @@
+import { useState, useEffect, useContext } from "react";
+import styles from "./NavBar.module.css";
+import { NavLink, useLocation } from "react-router-dom";
+
+const NavBar = () => {
+	const [scrolled, setScrolled] = useState(false);
+	const currentPath = useLocation().pathname;
+
+	const handleScroll = () => {
+		if (window.scrollY > 100) {
+			setScrolled(true);
+		} else {
+			setScrolled(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	return (
+		<nav
+			className={`${
+				styles["top-nav"]
+			} navbar navbar-expand-lg fixed-top navbar-dark ${
+				scrolled ? styles["new-top-nav"] : ""
+			}`}>
+			<div className="container topnav-container">
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div
+					className="collapse navbar-collapse"
+					id="navbarSupportedContent">
+					<ul className="navbar-nav ms-auto text-center">
+						<>
+							<li className="nav-item">
+								<a
+									className="nav-link custom-link text-light"
+									href="#explore">
+									Explore Luna
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className="nav-link custom-link text-light"
+									href="#about">
+									About
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className="nav-link custom-link text-light"
+									href="#contact">
+									Contact
+								</a>
+							</li>
+						</>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default NavBar;
