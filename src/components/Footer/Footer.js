d120bfff5f645debc266213690b79d35c6f36975
MohammadHelaly <mohammad.helaly@gmail.com>
2024-07-25 00:40:00 +0300

    Update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,67 +12,69 @@ const Footer = () => {
 			<div
 				ref={elementRef}
 				className={`container text-light ${styles["footer-container"]}`}>
-				<p className={styles["copyright"]}>
-					Background image by{" "}
-					<a
-						href="https://twitter.com/AJamesMcCarthy"
-						target="_blank"
-						rel="noreferrer">
-						Andrew McCarthy
-					</a>
-					.
-				</p>
-				<p className={styles["copyright"]}>
-					Surface of 3D moon globe from{" "}
-					<a
-						href="https://www.solarsystemscope.com/textures/"
-						target="_blank"
-						rel="noreferrer">
-						Solar System Scope
-					</a>
-					.
-				</p>
-				<p className={styles["copyright"]}>
-					Topography of 3D moon globe from{" "}
-					<a
-						href="https://astrogeology.usgs.gov/search/map/Moon/LMMP/LOLA-derived/Lunar_LRO_LOLA_ClrShade_Global_64ppd_BlueSteel"
-						target="_blank"
-						rel="noreferrer">
-						Moon LRO LOLA Shaded Relief (ESA)
-					</a>
-					.
-				</p>
-				<p className={styles["copyright"]}>
-					Seismic data gathered from{" "}
-					<a
-						href="https://pds-geosciences.wustl.edu/missions/apollo/seismic_event_catalog.htm"
-						target="_blank"
-						rel="noreferrer">
-						Apollo Seismic Event Catalog (NASA's PDS)
-					</a>
-					.
-				</p>
-				<p className={styles["copyright"]}>
-					Virtual Experience developed with{" "}
-					<a
-						href="https://www.unrealengine.com/en-US/"
-						target="_blank"
-						rel="noreferrer">
-						Unreal Engine 5
-					</a>
-					.
-				</p>
-				<p className={styles["copyright"]}>
-					Developed with{" "}
-					<a
-						href="https://reactjs.org/"
-						target="_blank"
-						rel="noreferrer">
-						React.js
-					</a>
-					.
-				</p>
-				<p className={styles["source-code-link"]}>
+				<div className={styles["footer-content"]}>
+					<div>
+						<p className={styles["copyright"]}>
+							Background image by{" "}
+							<a
+								href="https://twitter.com/AJamesMcCarthy"
+								target="_blank"
+								rel="noreferrer">
+								Andrew McCarthy
+							</a>
+							.
+						</p>
+						<p className={styles["copyright"]}>
+							Surface of 3D moon globe from{" "}
+							<a
+								href="https://www.solarsystemscope.com/textures/"
+								target="_blank"
+								rel="noreferrer">
+								Solar System Scope
+							</a>
+							.
+						</p>
+						<p className={styles["copyright"]}>
+							Topography of 3D moon globe from{" "}
+							<a
+								href="https://astrogeology.usgs.gov/search/map/Moon/LMMP/LOLA-derived/Lunar_LRO_LOLA_ClrShade_Global_64ppd_BlueSteel"
+								target="_blank"
+								rel="noreferrer">
+								Moon LRO LOLA Shaded Relief (ESA)
+							</a>
+							.
+						</p>
+						<p className={styles["copyright"]}>
+							Seismic data gathered from{" "}
+							<a
+								href="https://pds-geosciences.wustl.edu/missions/apollo/seismic_event_catalog.htm"
+								target="_blank"
+								rel="noreferrer">
+								Apollo Seismic Event Catalog (NASA's PDS)
+							</a>
+							.
+						</p>
+						<p className={styles["copyright"]}>
+							Virtual Experience developed with{" "}
+							<a
+								href="https://www.unrealengine.com/en-US/"
+								target="_blank"
+								rel="noreferrer">
+								Unreal Engine 5
+							</a>
+							.
+						</p>
+						<p className={styles["copyright"]}>
+							Developed with{" "}
+							<a
+								href="https://reactjs.org/"
+								target="_blank"
+								rel="noreferrer">
+								React.js
+							</a>
+							.
+						</p>
+					</div>
 					<Icon
 						link={true}
 						to="https://www.github.com/MohammadHelaly/lunar-trek">
@@ -83,7 +85,7 @@ const Footer = () => {
 							className={`${styles["source-code-icon"]}`}
 						/>
 					</Icon>
-				</p>
+				</div>
 				<hr />
 				<p className={styles["copyright"]}>&copy; {currentYear}</p>
 			</div>
